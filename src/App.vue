c52d5bec4730b915a6df278c1beebdd0d5606a40
kovalchyk.y <kovalchyk.y@hexide-digital.com>
2025-03-07 21:37:04 +0200

    feat: initiall commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header class="header">
+    <div class="app-container">
+      Hello from Header
+    </div>
+  </header>
+  <main class="main">
+    <div class="app-container">
+      Hello from Main
+    </div>
+  </main>
+  <footer class="footer">
+    <div class="app-container">
+      Hello from Footer
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
